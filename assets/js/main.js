78161986f444d513463dd2792375effcdd6f735f
Semburg <semburg.sb@gmail.com>
2021-02-10 17:53:59 +0100

    added ENter click in TextForm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
-function sendMessage(){
+
+
+function sendMessage() {
     let mess = document.getElementById("messageText").value
     var doc = document.getElementById("sentText");
     var errorMessage = document.getElementById("emptyField");
 
 
-
-//?    any other method for first "if"?   null/ undefined/ empty was not working
-    if (mess.length == 0){
+    //?    any other method for first "if"?   null/ undefined/ empty was not working
+    if (mess.length == 0) {
         doc.innerHTML = "";
         errorMessage.innerHTML = "Gib Nachricht ein!"
     } else {
         errorMessage.innerHTML = ""
         doc.innerHTML = mess;
     }
+}
+
+// *      On Enter Click in form:text.   usability etc...
+function onEnter(event){
+    var code = 0;
+    code = event.keyCode;
+    
+    if(code==13){
+        sendMessage();
+        event.preventDefault();
+    }
+    
 }
